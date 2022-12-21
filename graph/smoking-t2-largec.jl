183194d8f54be89da10fdd82e066c0feada7977d
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-12-21 03:53:02 +0200

    Output graphs for `-t 2`, large C

---
+++ graph/smoking-t2-largec.jl
@@ -0,0 +1,21 @@
+using DataFrames, CSV
+using Plots
+
+skipped = 6;
+
+filename = "logs/smoking-t2.log";
+df = DataFrame(CSV.File(filename, header=1));
+nrows, ncols = size(df);
+df = last(df, skipped);
+nrows = skipped;
+
+p = Plots.scatter(
+    title = "g = 0.04",
+    xlabel = "C",
+    ylabel = "Accuracy[%]",
+    df[!, :c], df[!, :acc],
+    markersize = 4,
+    markercolor = :black,
+    legend = false
+);
+savefig(p, "image/smoking-t2/smoking-t2-largec.png");
